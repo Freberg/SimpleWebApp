6d38b85ccc0fd2125e6d485246ab49636af5392a
Daniel Freberg <daniel.freberg@scila.se>
2020-08-10 20:39:32 +0200

    Added temporary workaround for spring integration tests not terminating

--- src/test/java/com/freberg/app/controller/MovieControllerIntegrationTest.java
+++ src/test/java/com/freberg/app/controller/MovieControllerIntegrationTest.java
@@ -3,17 +3,19 @@ package com.freberg.app.controller;
 
 import java.util.List;
 
-import com.freberg.app.SimpleWebApp;
 import com.freberg.app.entity.Movie;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SimpleWebApp.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest()
+// Todo - Remove @DirtiestContext annotation, currently the tests are not finishing without it, figure out why
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MovieControllerIntegrationTest {
 
     @Autowired
